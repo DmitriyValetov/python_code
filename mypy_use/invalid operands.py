cdc8e9cffde4be78cfe44a673fb9bc60626e8045
DmitriyValetov <dmitriy.valetov@phystech.edu>
2018-11-05 16:48:31 +0300

    added mypy some use

---
+++ mypy_use/invalid operands.py
@@ -0,0 +1,8 @@
+"""
+here is a simple `error` with (+ int str)
+"""
+
+a = 'abc' + 'def'
+b = 1
+c = '1'
+d = b + c
---
+++ mypy_use/type_defining.py
@@ -0,0 +1,9 @@
+"""
+here is straight type defining
+"""
+
+
+def greeting(name: str) -> str:
+    return 'Hello ' + name
+
+greeting(5)
